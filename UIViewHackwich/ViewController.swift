359985c4210b3fa0b6bf08aa76c6a5cd15897783
ISRAEL TEJEDA <rl106765@rlas-macair-15.local>
2018-12-13 11:34:01 -0600

    Initial Commit

---
+++ UIViewHackwich/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIViewHackwich
+//
+//  Created by ISRAEL TEJEDA on 10/5/18.
+//  Copyright © 2018 ISRAEL TEJEDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var newView = UIView(frame: CGRect(x: 20, y: 50, width: 150,height: 150))
+        view.addSubview(newView)
+        view.
+    }
+
+
+
+
+}
+
